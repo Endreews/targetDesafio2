c154c16790b86186d896fda568c31bcb21f46338
Endreews <150464197+Endreews@users.noreply.github.com>
2024-03-08 14:43:37 -0300

    Update DesafioFibonacci.java

--- src/DesafioFibonacci.java
+++ src/DesafioFibonacci.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class DesafioFibonacci {
-}
+import java.util.Scanner;
+
+public class DesafioFibonacci {
+    public static void main(String[] args) {
+       //Criar Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escreva um numero abaixo:");
+        int fibonacci = scanner.nextInt();
+
+        if(scannerFibonati(fibonacci)) {
+            System.out.println("(" + fibonacci + ")" + "\sPertence a sequencia de Fibonacci!");
+        } else {
+            System.out.println("Não pertence!");
+        }
+        scanner.close();
+    }
+    private static boolean scannerFibonati(int fibonacci) {
+        if(fibonacci <= 1) {
+            return fibonacci == 1;
+        }
+        int f = 0, i = 1;
+
+        while (i <= fibonacci) {
+            if (i == fibonacci)
+                return true;
+        }
+        int looping = f + i;
+        f = i;
+        i = looping;
+        return false;
+        }
+    }
